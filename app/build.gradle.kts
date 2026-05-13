813709dbee758a731ffc3cfe342d1e3c27288e0a
Yosri <yosrri11@gmail.com>
2026-05-13 15:04:53 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -74,6 +74,8 @@ dependencies {
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
 
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.5.1")
+
     debugImplementation("androidx.compose.ui:ui-test-manifest")
     debugImplementation("androidx.compose.ui:ui-tooling")
 }
